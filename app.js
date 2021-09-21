1153aed8cdb4eba7fc14d9b4cfb9aad4515a633c
EnsayistaPop <pablo.producer21@gmail.com>
2021-09-21 11:09:36 -0500

    ultima actua

--- app.js
+++ app.js
@@ -19,3 +19,7 @@ app.get('/', (req, res) => {
 app.get('/registro', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'views/registro.html'))
 });
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'views/login.html'))
+});
